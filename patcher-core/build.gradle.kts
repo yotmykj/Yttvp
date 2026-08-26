ead9377fda38332cacb6d22a8661d6b123899611
Manas <saparbektv@gmail.com>
2026-08-26 16:29:40 +0500

    Replace specific Kotlin stdlib version with plugin reference

--- patcher-core/build.gradle.kts
+++ patcher-core/build.gradle.kts
@@ -1,7 +1,11 @@
+plugins {
+    kotlin("jvm")
+}
+
 dependencies {
     implementation(project(":common"))
     implementation(project(":patcher-api"))
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.23")
+    implementation(kotlin("stdlib"))
     implementation("com.android.tools.build:apksig:8.2.2")
     implementation("com.android.tools:common:31.2.2")
 }
