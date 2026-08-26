bb95a8fd355ba0faaee29d3be77a9c078f00b346
Manas <saparbektv@gmail.com>
2026-08-26 15:28:09 +0500

    Modified by www.SourceFiles.app

---
+++ patcher-core/build.gradle.kts
@@ -0,0 +1,7 @@
+dependencies {
+    implementation(project(":common"))
+    implementation(project(":patcher-api"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.23")
+    implementation("com.android.tools.build:apksig:8.2.2")
+    implementation("com.android.tools:common:31.2.2")
+}
